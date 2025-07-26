6446014e07920613fae39477ffd3fa6aee82136e
poorm <315luz10@gmail.com>
2025-07-26 16:06:53 +0900

    init

---
+++ src/main/java/com/spartatest/SpartatestApplication.java
@@ -0,0 +1,13 @@
+package com.spartatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spartatest/SpartatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
